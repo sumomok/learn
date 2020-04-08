cd8d4b919e19adb0ba8a679db09faa2ec865ae86
sumomok <54304732+sumomok@users.noreply.github.com>
2020-04-08 19:20:22 +0800

    ts项目

---
+++ typeScript/movieEsys/Readme.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-04-08 16:26:16
+ * @LastEditTime: 2020-04-08 16:26:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \learn\typeScript\movieEsys\Readme.md
+ -->
---
+++ typeScript/movieEsys/server/src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-08 16:42:51
+ * @LastEditTime: 2020-04-08 16:46:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \learn\typeScript\movieEsys\server\src\index.ts
+ */
+let testa: string = "fasdf"
+console.log("sdafasdf");
